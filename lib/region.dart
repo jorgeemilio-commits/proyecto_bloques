d72bf189e11c4f010cc24b85385a4619cca71d48
emilio_com <jorgeemilio7777@gmail.com>
2026-09-17 07:35:04 -0700

    Se agregaron las coordenadas de las regiones de acuerdo al nivel 1 de Brilliant

--- lib/region.dart
+++ lib/region.dart
@@ -45,8 +45,9 @@ List<int> obtenerNumerosRegion(
 
 abstract class Region {
   final TipoRegion tipo;
+  final List<Coordenada> coordenadas;
 
-	const Region(this.tipo);
+	const Region(this.tipo, this.coordenadas);
 }
 
 class Coordenada {
@@ -56,16 +57,122 @@ class Coordenada {
   const Coordenada(this.x, this.y);
 }
 
+// Regiones y coordenadas
 // super permite que se pueda crear una region de un tipo especifico (por ejemplo, RegionAzulUno hijo de tipo azul y herada sus caracteristicas)
 class RegionAzulUno extends Region {
-	const RegionAzulUno() : super(TipoRegion.azul);
+	const RegionAzulUno()
+			: super(TipoRegion.azul, const [
+					Coordenada(2, 0),
+					Coordenada(2, 1),
+					Coordenada(3, 1),
+					Coordenada(3, 2),
+				]);
 }
 
 class RegionAzulDos extends Region {
-	const RegionAzulDos() : super(TipoRegion.azul);
+	const RegionAzulDos()
+			: super(TipoRegion.azul, const [
+					Coordenada(6, 4),
+					Coordenada(5, 5),
+					Coordenada(6, 5),
+					Coordenada(5, 6),
+				]);
 }
 
+class RegionRojaUno extends Region {
+	const RegionRojaUno()
+			: super(TipoRegion.rojo, const [
+					Coordenada(1, 2),
+					Coordenada(2, 2),
+					Coordenada(1, 3),
+					Coordenada(1, 4),
+					Coordenada(0, 5),
+					Coordenada(1, 5),
+				]);
+}
 
+class RegionRojaDos extends Region {
+	const RegionRojaDos()
+			: super(TipoRegion.rojo, const [
+					Coordenada(4, 4),
+					Coordenada(5, 4),
+					Coordenada(3, 5),
+					Coordenada(4, 5),
+					Coordenada(3, 6),
+					Coordenada(4, 6),
+				]);
+}
+
+class RegionVerdeUno extends Region {
+	const RegionVerdeUno()
+			: super(TipoRegion.verde, const [
+					Coordenada(1, 0),
+					Coordenada(0, 1),
+					Coordenada(1, 1),
+					Coordenada(0, 2),
+					Coordenada(0, 3),
+					Coordenada(0, 4),
+				]);
+}
+
+class RegionVerdeDos extends Region {
+	const RegionVerdeDos()
+			: super(TipoRegion.verde, const [
+					Coordenada(6, 1),
+					Coordenada(5, 2),
+					Coordenada(6, 2),
+					Coordenada(4, 3),
+					Coordenada(5, 3),
+					Coordenada(6, 3),
+				]);
+}
+
+class RegionLilaUno extends Region {
+	const RegionLilaUno()
+			: super(TipoRegion.lila, const [
+					Coordenada(3, 0),
+					Coordenada(4, 0),
+					Coordenada(5, 0),
+					Coordenada(4, 1),
+					Coordenada(5, 1),
+					Coordenada(4, 2),
+				]);
+}
+
+class RegionLilaDos extends Region {
+	const RegionLilaDos()
+			: super(TipoRegion.lila, const [
+					Coordenada(2, 3),
+					Coordenada(2, 4),
+					Coordenada(3, 4),
+					Coordenada(2, 5),
+					Coordenada(1, 6),
+					Coordenada(2, 6),
+				]);
+}
+
+class RegionAmarilla extends Region {
+	const RegionAmarilla()
+			: super(TipoRegion.amarillo, const [
+					Coordenada(0, 0),
+					Coordenada(6, 0),
+					Coordenada(3, 3),
+					Coordenada(0, 6),
+					Coordenada(6, 6),
+				]);
+}
+
+const List<Region> regionesMapa = [
+	RegionAzulUno(),
+	RegionAzulDos(),
+	RegionRojaUno(),
+	RegionRojaDos(),
+	RegionVerdeUno(),
+	RegionVerdeDos(),
+	RegionLilaUno(),
+	RegionLilaDos(),
+	RegionAmarilla(),
+];
 
 
 //cual sistema de coordenadas se usa para el tablero
