10e0d1feb2aaa65330c1dacd8679f256c7c53467
emilio_com <jorgeemilio7777@gmail.com>
2026-09-21 08:04:27 -0700

    Se quito la region de la celda

--- lib/tablero.dart
+++ lib/tablero.dart
@@ -2,12 +2,10 @@ import 'region.dart';
 
 // Clase para representar una celda del tablero
 class Celda {
-  final Region? region;
   final Coordenada coordenada;
   int? numero;
 
   Celda({
-    this.region,
     required this.coordenada,
     this.numero,
   });
@@ -16,41 +14,41 @@ class Celda {
 class Tablero {
   final int filas;
   final int columnas;
+  final List<Region> regiones;
   final List<List<Celda>> celdas;
 
   Tablero.desdeRegiones({
     required this.filas,
     required this.columnas,
     required List<Region> regiones,
-  }) : celdas = _crearCeldas(filas, columnas, regiones);
+  })  : regiones = List.unmodifiable(regiones),
+        celdas = _crearCeldas(filas, columnas);
+
+  Region? obtenerRegion(Coordenada coordenada) {
+    for (final region in regiones.reversed) {
+      final coordenadaPerteneceARegion = region.coordenadas.any(
+        (coordenadaDeRegion) =>
+            coordenadaDeRegion.x == coordenada.x &&
+            coordenadaDeRegion.y == coordenada.y,
+      );
+
+      if (coordenadaPerteneceARegion) {
+        return region;
+      }
+    }
+
+    return null;
+  }
 
   static List<List<Celda>> _crearCeldas(
     int filas,
     int columnas,
-    List<Region> regiones,
   ) {
-    final celdas = List.generate(
-      filas,
-      (_) => List<Celda?>.filled(columnas, null),
-    );
-
-    for (final region in regiones) {
-      for (final coordenada in region.coordenadas) {
-        celdas[coordenada.y][coordenada.x] = Celda(
-          region: region,
-          coordenada: coordenada,
-        );
-      }
-    }
-
     return [
       for (int y = 0; y < filas; y++)
         [
           for (int x = 0; x < columnas; x++)
-            celdas[y][x] ??
-                Celda(
-                  coordenada: Coordenada(x, y),
-                ),
+            Celda(coordenada: Coordenada(x, y)),
         ],
     ];
   }
